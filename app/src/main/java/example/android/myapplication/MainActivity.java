3ee1d87f0643fc3929ce7c7c0db7018e60a9392f
Shintaku <ukyankyan11@gmail.com>
2015-11-06 22:58:51 +0900

    test

--- app/src/main/java/example/android/myapplication/MainActivity.java
+++ app/src/main/java/example/android/myapplication/MainActivity.java
@@ -1,13 +1,22 @@
 package example.android.myapplication;
 
-import android.support.v7.app.AppCompatActivity;
+import android.app.Activity;
 import android.os.Bundle;
+import android.view.animation.TranslateAnimation;
+import android.widget.ImageView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends Activity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        int cell = 5;
+        ImageView img = (ImageView) findViewById(R.id.imageView2);
+        TranslateAnimation translate = new TranslateAnimation(0, 150*cell, 0, 0);
+        translate.setDuration(1500); // 3000msかけてアニメーションする
+        translate.setFillAfter(true);
+        img.startAnimation(translate); // アニメーション適用
     }
 }
